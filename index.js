2ec4b50b66876ff586b29b4e327d34c83dce1a88
Brandon Pate <bepate@att.net>
2018-05-24 21:44:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function happyHolidays(name) {
 }
 
 function happyHolidaysTo(name) {
-  return (`Happy holidays, ${name}!`)
+  return (`Happy holidays, ${name}!`);
 }
 
 function happyHolidayTo(holiday, name) {
-  return(`Happy ${holiday}, ${name}!)
+  return(`Happy ${holiday}, ${name}!);
 }
